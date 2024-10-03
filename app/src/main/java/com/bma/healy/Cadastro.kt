425869349ecda5376ce111f714e05479f0181c81
aaaaaaaaana <rm551401@fiap.com.br>
2024-10-03 12:11:46 -0300

    tentando arrumar tudo

--- app/src/main/java/com/bma/healy/Cadastro.kt
+++ app/src/main/java/com/bma/healy/Cadastro.kt
@@ -1,20 +1,37 @@
 package com.bma.healy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Cadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_cadastro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        var linkCadastro: TextView  = findViewById(R.id.linkCadastro)
+
+        var botaoCadastro: TextView = findViewById(R.id.botaoCadastro)
+
+
+        // adicionar evento do clique
+        linkCadastro.setOnClickListener{
+
+            var intent = Intent(this, MainActivity::class.java)
+
+            startActivity(intent)
+
+        }
+
+        botaoCadastro.setOnClickListener{
+
+            var intent = Intent(this, MainActivity::class.java)
+
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/bma/healy/MainActivity.kt
+++ app/src/main/java/com/bma/healy/MainActivity.kt
@@ -1,20 +1,62 @@
 package com.bma.healy
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.bma.healy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+
+    lateinit var username: EditText
+    lateinit var password: EditText
+    lateinit var loginButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.botaoLogin.setOnClickListener(View.OnClickListener {
+            if(binding.user.text.toString() == "user" && binding.senha.text.toString() == "1234"){
+                Toast.makeText(this, "Logado com Sucesso", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Falha no Login", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+
+        var linkLogin: TextView = findViewById(R.id.linkLogin)
+
+        var botaoLogin: TextView = findViewById(R.id.botaoLogin)
+
+
+        // adicionar evento do clique
+        linkLogin.setOnClickListener {
+
+            var intent = Intent(this, Cadastro::class.java)
+
+            startActivity(intent)
         }
+
+        botaoLogin.setOnClickListener {
+
+            var intent = Intent(this, Home::class.java)
+
+            startActivity(intent)
+        }
+
+
     }
 }
