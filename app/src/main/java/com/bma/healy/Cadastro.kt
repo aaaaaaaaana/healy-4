772dff72328e0d395781bcac66dd5a646cbc62c0
aaaaaaaaana <rm551401@fiap.com.br>
2024-10-23 11:25:29 -0300

    haha

--- app/src/main/java/com/bma/healy/Cadastro.kt
+++ app/src/main/java/com/bma/healy/Cadastro.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 
-class Cadastro : AppCompatActivity() {
+class Cadastro : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cadastro)
+        setContentView(R.layout.fragment_cadastro)
 
 
         var linkCadastro: TextView  = findViewById(R.id.linkCadastro)
--- app/src/main/java/com/bma/healy/MainActivity.kt
+++ app/src/main/java/com/bma/healy/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -15,53 +16,22 @@ import com.bma.healy.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
 
-    private lateinit var binding: ActivityMainBinding
-
-    lateinit var username: EditText
-    lateinit var password: EditText
-    lateinit var loginButton: Button
+    private lateinit var botaoLogin: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
 
-        val menu = findNavController(R.id.home)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        botaoLogin = findViewById(R.id.botaoLogin)
 
 
-
-        binding.botaoLogin.setOnClickListener(View.OnClickListener {
-            if(binding.user.text.toString() == "user" && binding.senha.text.toString() == "1234"){
-                Toast.makeText(this, "Logado com Sucesso", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Falha no Login", Toast.LENGTH_SHORT).show()
-            }
-        })
-
-
-        var linkLogin: TextView = findViewById(R.id.linkLogin)
-
-        var botaoLogin: TextView = findViewById(R.id.botaoLogin)
-
-
-        // adicionar evento do clique
-        linkLogin.setOnClickListener {
-
-            var intent = Intent(this, Cadastro::class.java)
-
-            startActivity(intent)
-        }
+        showFragment(ListaFragment())
 
         botaoLogin.setOnClickListener {
-
-            var intent = Intent(this, Home::class.java)
-
-            startActivity(intent)
+            showFragment(CadastroFragment())
         }
-
-
     }
+
 }
