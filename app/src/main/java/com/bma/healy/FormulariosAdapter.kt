57d195c0de20979b058ae3c82fcff6787875cd73
aaaaaaaaaana <rm551401@fiap.com.br>
2024-10-24 12:40:01 -0300

    fazendo muitas coisas

---
+++ app/src/main/java/com/bma/healy/FormulariosAdapter.kt
@@ -0,0 +1,48 @@
+package com.bma.healy
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+
+import com.bma.healy.placeholder.PlaceholderContent.PlaceholderItem
+import com.bma.healy.databinding.FragmentItemBinding
+
+/**
+ * [RecyclerView.Adapter] that can display a [PlaceholderItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class FormulariosAdapter(
+    private val values: List<PlaceholderItem>
+) : RecyclerView.Adapter<FormulariosAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            FragmentItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = item.id
+        holder.contentView.text = item.content
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: FragmentItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        val idView: TextView = binding.itemNumber
+        val contentView: TextView = binding.content
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+
+}
