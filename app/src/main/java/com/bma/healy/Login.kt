58a1479015706e6c20ca8ccab6977c3cd0af78fc
aaaaaaaaana <rm551401@fiap.com.br>
2024-10-28 11:43:10 -0300

    ta erradinho (mas so por enquanto

--- app/src/main/java/com/bma/healy/Login.kt
+++ app/src/main/java/com/bma/healy/Login.kt
@@ -27,7 +27,7 @@ class Login : AppCompatActivity() {
         }
 
         linkLogin.setOnClickListener {
-            showFragment(Cadastr())
+            showFragment(Cadastro())
         }
     }
 
