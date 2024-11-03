14ad45f08d77588afd0acac8604dda45330d582c
aaaaaaaaaana <rm551401@fiap.com.br>
2024-11-03 18:27:43 -0300

    arrumando esqueceu senha

--- app/src/main/java/com/bma/healy/EsqueceuSenha.kt
+++ app/src/main/java/com/bma/healy/EsqueceuSenha.kt
@@ -11,21 +11,17 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
-
+import com.google.firebase.auth.FirebaseAuth
 
 class EsqueceuSenha : Fragment() {
 
     private val autenticador by lazy {
-        Firebase.auth
+        FirebaseAuth.getInstance()
     }
-
 
     private lateinit var linkEsqueceuSenha: TextView
     private lateinit var botaoRedefinir: Button
     private lateinit var email: EditText
-    private lateinit var novaSenha: EditText
-    private lateinit var confirmaSenha: EditText
 
 
     override fun onCreateView(
@@ -42,97 +38,39 @@ class EsqueceuSenha : Fragment() {
         linkEsqueceuSenha = view.findViewById(R.id.linkEsqueceu)
         botaoRedefinir = view.findViewById(R.id.botaoRedefinir)
         email = view.findViewById(R.id.emailEsqueceu)
-        novaSenha = view.findViewById(R.id.novaSenha)
-        confirmaSenha = view.findViewById(R.id.confirmaSenhaNova)
-
         linkEsqueceuSenha.setOnClickListener {
             (activity as MainActivity).showFragment(Login())
         }
 
         botaoRedefinir.setOnClickListener {
             val email = email.text.toString().trim()
-            val novaSenha = novaSenha.text.toString().trim()
-            val confirmaSenha = confirmaSenha.text.toString().trim()
 
             if (email.isEmpty()) {
                 Toast.makeText(context, "Digite seu email", Toast.LENGTH_SHORT).show()
             } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 Toast.makeText(context, "Email inválido", Toast.LENGTH_SHORT).show()
-            } else if (novaSenha.isEmpty() || confirmaSenha.isEmpty()) {
-                Toast.makeText(context, "Digite a nova senha e a confirmação", Toast.LENGTH_SHORT)
-                    .show()
-            } else if (novaSenha != confirmaSenha) {
-                Toast.makeText(context, "Senhas não coincidem", Toast.LENGTH_SHORT).show()
             } else {
 
-                autenticador.fetchSignInMethodsForEmail(email)
+                autenticador.sendPasswordResetEmail(email)
                     .addOnCompleteListener { task ->
                         if (task.isSuccessful) {
+                            Toast.makeText(
+                                context,
+                                "Email de redefinição de senha enviado!",
+                                Toast.LENGTH_SHORT
+                            ).show()
 
-                            atualizarSenha(email, novaSenha)
+                            (activity as MainActivity).showFragment(Login())
                         } else {
 
-                            Toast.makeText(context, "Email não encontrado.", Toast.LENGTH_SHORT)
-                                .show()
+                            Toast.makeText(
+                                context,
+                                "Erro ao enviar email de redefinição de senha.",
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
-                    }
-                    .addOnFailureListener { exception ->
-
-                        Toast.makeText(
-                            context,
-                            "Erro ao verificar o email: ${exception.message}",
-                            Toast.LENGTH_SHORT
-                        ).show()
                     }
             }
         }
-    }
-
-
-    private fun atualizarSenha(email: String, novaSenha: String) {
-        autenticador.signInWithEmailAndPassword(email, "")
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-
-                    val user = task.result?.user
-                    if (user != null) {
-                        user.updatePassword(novaSenha)
-                            .addOnCompleteListener { updateTask ->
-                                if (updateTask.isSuccessful) {
-
-                                    Toast.makeText(
-                                        context,
-                                        "Senha atualizada com sucesso!",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-
-                                    (activity as MainActivity).showFragment(Login())
-                                } else {
-
-                                    Toast.makeText(
-                                        context,
-                                        "Erro ao atualizar a senha: ${updateTask.exception?.message}",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
-                            }
-                            .addOnFailureListener { exception ->
-
-                                Toast.makeText(
-                                    context,
-                                    "Erro ao atualizar a senha: ${exception.message}",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                    }
-                } else {
-
-                    Toast.makeText(
-                        context,
-                        "Erro ao fazer login: ${task.exception?.message}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
     }
 }
